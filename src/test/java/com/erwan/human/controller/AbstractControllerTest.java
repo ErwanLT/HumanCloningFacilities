2f2c3637aaec222c924020c00019331c9f491b66
Erwan <erwanletutour.elt@gmail.com>
2022-02-09 11:10:28 +0100

    refactor tests

--- src/test/java/com/erwan/human/controller/AbstractControllerTest.java
+++ src/test/java/com/erwan/human/controller/AbstractControllerTest.java
@@ -1,8 +1,11 @@
 package com.erwan.human.controller;
 
 import com.erwan.human.dao.CloneRepository;
+import com.erwan.human.domaine.Clone;
+import com.erwan.human.reference.CloneType;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.openapitools.client.api.JediControllerApi;
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -10,6 +13,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Locale;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -27,4 +32,23 @@ public class AbstractControllerTest {
 
     @MockBean
     protected JediControllerApiImpl jediControllerApi;
+
+    protected static Faker faker;
+
+    @BeforeAll
+    public static void setUp(){
+
+        faker = new Faker(Locale.FRENCH);
+
+    }
+
+
+    public Clone getClone(Long id) {
+        return Clone.builder()
+                .id(faker.random().nextLong())
+                .codeName(faker.elderScrolls().lastName())
+                .type(CloneType.flametrooper)
+                .affiliation(faker.company().name())
+                .build();
+    }
 }
